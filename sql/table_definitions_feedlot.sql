5e43bf8c2ed117817aa4e1ceef8be6cc80f6ed45
Grant Person <grant.lee.person@gmail.com>
2016-01-05 20:26:44 -0600

    Create table files

---
+++ sql/table_definitions_feedlot.sql
@@ -0,0 +1,131 @@
+DROP DATABASE IF EXISTS feedlot;
+CREATE DATABASE feedlot;
+USE feedlot;
+
+#######################################################################
+############			TABLES HERD						###############
+#######################################################################
+CREATE TABLE HERD (
+	`herdId`						INT UNIQUE NOT NULL AUTO_INCREMENT,
+    `quantity`						INT NOT NULL,
+    `weight`						INT NOT NULL,
+    `cost`							DOUBLE NOT NULL,
+    `tagNumber`						VARCHAR(15) NOT NULL,
+    `estimatedSaleDate`				DATETIME,
+    `implantDate`					DATETIME,
+    `optiflexDate`					DATETIME,
+    `dateEntered`					DATETIME NOT NULL,
+    PRIMARY KEY ( herdId )
+) ENGINE = InnoDB;
+
+CREATE TABLE SUPPLIER (
+	`supplierId`					INT UNIQUE NOT NULL AUTO_INCREMENT,
+    `supplierName`					VARCHAR(100),
+    PRIMARY KEY ( supplierId )
+) ENGINE = InnoDB;
+
+CREATE TABLE HERD_SUPPLIERS_MAP (
+	`herdSupplierId`				INT UNIQUE NOT NULL AUTO_INCREMENT,
+	`supplierId`					INT NOT NULL,
+	`herdId`						INT NOT NULL,
+	PRIMARY KEY ( herdSupplierId ),
+	FOREIGN KEY ( supplierId ) REFERENCES SUPPLIER( supplierId ),
+	FOREIGN KEY ( herdId ) REFERENCES HERD( herdId )
+) ENGINE = InnoDB;
+
+CREATE TABLE LOCALE (
+	`localeId`						INT UNIQUE NOT NULL AUTO_INCREMENT,
+    `localeName`					VARCHAR(50) NOT NULL
+) ENGINE = InnoDB;
+
+CREATE TABLE HERD_LOCALE_MAP (
+	`herdLocaleId`					INT UNIQUE NOT NULL AUTO_INCREMENT,
+	`localeId`						INT NOT NULL,
+	`herdId`						INT NOT NULL,
+    `dateEnteringLocale`			DATETIME NOT NULL,
+	PRIMARY KEY ( herdLocaleId ),
+	FOREIGN KEY ( localeId ) REFERENCES LOCALE( localeId ),
+	FOREIGN KEY ( herdId ) REFERENCES HERD( herdId )
+) ENGINE = InnoDB;
+
+CREATE TABLE SALE (
+	`saleId`						INT UNIQUE NOT NULL AUTO_INCREMENT,
+    `herdId`						INT NOT NULL,
+    `salePrice`						DOUBLE NOT NULL,
+    `saleWeight`					INT NOT NULL,
+    `quantity`						INT NOT NULL,
+    `saleDate`						DATETIME NOT Null,
+    PRIMARY KEY ( saleId ),
+	FOREIGN KEY ( herdId ) REFERENCES HERD( herdId )
+) ENGINE = InnoDB;
+
+
+#######################################################################
+############			TABLES Users					###############
+#######################################################################
+CREATE TABLE ROLES (
+	`roleId`						INT UNIQUE NOT NULL AUTO_INCREMENT,
+    `roleType`						INT NOT NULL,
+    PRIMARY KEY ( roleId )
+) ENGINE = InnoDB;
+    
+CREATE TABLE FARM (
+	`farmId`						INT UNIQUE NOT NULL AUTO_INCREMENT,
+    `farmName`						VARCHAR(50),
+    PRIMARY KEY ( farmId )
+) ENGINE = InnoDB;
+
+CREATE TABLE USERS (
+	`userId`						INT UNIQUE NOT NULL AUTO_INCREMENT,
+    `userName`						VARCHAR(25) NOT NULL,
+    `firstName`						VARCHAR(25) NOT NULL,
+	`lastName`						VARCHAR(25) NOT NULL,
+	`email`							VARCHAR(50) NOT NULL,
+	`password`						VARCHAR(100) NOT NULL,
+	`farmId`						INT NOT NULL,
+	`roleId`						INT NOT NULL,
+	PRIMARY KEY ( userId ),
+    FOREIGN KEY ( farmId ) REFERENCES FARM( farmId ),
+    FOREIGN KEY ( roleId ) REFERENCES ROLES( roleId )
+) ENGINE = InnoDB;
+
+
+#######################################################################
+############			TABLES Feeding					###############
+#######################################################################
+CREATE TABLE FEED_TYPES (
+	`feedTypeId`					INT UNIQUE NOT NULL AUTO_INCREMENT,
+    `feedType` 						VARCHAR(25) NOT NULL,
+    PRIMARY KEY ( feedTypeId )
+) ENGINE = InnoDB;
+
+CREATE TABLE FEED_STOCK (
+	`feedStockId` 					INT UNIQUE NOT NULL AUTO_INCREMENT,
+	`feedTypeId`					INT NOT NULL,
+    `stockAmount` 					INT NOT NULL,
+    PRIMARY KEY ( feedStockId ),
+    FOREIGN KEY ( feedTypeId ) REFERENCES FEED_TYPES( feedTypeId )
+) ENGINE = InnoDB;
+
+CREATE TABLE FEEDING (
+	`feedingId`						INT UNIQUE NOT NULL AUTO_INCREMENT,
+	`feedingTime`					INT NOT NULL,
+    `feedingDate`					DATETIME NOT Null,
+	`localeId` 						INT NOT NULL,
+	`bunckScore`					INT NOT NULL,
+	`deliveredAmountTMRD`			INT NOT NULL,
+	`userId`						INT NULL,
+    PRIMARY KEY ( feedingId ),
+    FOREIGN KEY ( localeId ) REFERENCES LOCALE( localeId ),
+    FOREIGN KEY ( userId ) REFERENCES USERS( userId )
+) ENGINE = InnoDB;
+
+CREATE TABLE FEEDS (
+	`feedId` 						INT UNIQUE NOT NULL AUTO_INCREMENT,
+	`feedTypeId` 					INT NOT NULL,
+	`feedAmount`					INT NOT NULL,
+	`feedingId`						INT NOT NULL,
+	PRIMARY KEY ( feedId ),
+    FOREIGN KEY ( feedTypeId ) REFERENCES FEED_TYPES( feedTypeId ),
+    FOREIGN KEY ( feedingId ) REFERENCES FEEDING( feedingId )
+) ENGINE = InnoDB;
