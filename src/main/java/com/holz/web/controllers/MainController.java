35ca3c3ca32c77f8ba9cd3b4185f252fd22dd1a5
Grant Person <grant.lee.person@gmail.com>
2016-01-08 20:04:14 -0600

    Missed files

---
+++ src/main/java/com/holz/web/controllers/MainController.java
@@ -0,0 +1,43 @@
+package com.holz.web.controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.servlet.ModelAndView;
+
+@Controller
+public class MainController {
+	
+	@RequestMapping(value = { "/", "/home**" }, method = RequestMethod.GET)
+	public ModelAndView Home() {
+		ModelAndView model = new ModelAndView("manager.home");
+		model.addObject("title", "Welcome");
+		model.addObject("message", "This is default page!");
+		return model;
+	}
+	
+	@RequestMapping(value = { "/mixFeed**" }, method = RequestMethod.GET)
+	public ModelAndView MixFeed() {
+		ModelAndView model = new ModelAndView("manager.mixFeed");
+		model.addObject("title", "Welcome");
+		model.addObject("message", "This is default page!");
+		return model;
+	}
+	
+	@RequestMapping(value = { "/deliver**" }, method = RequestMethod.GET)
+	public ModelAndView Deliver() {
+		ModelAndView model = new ModelAndView("manager.deliver");
+		model.addObject("title", "Welcome");
+		model.addObject("message", "This is default page!");
+		return model;
+	}
+	
+	@RequestMapping(value = { "/pickLocation**" }, method = RequestMethod.GET)
+	public ModelAndView PickLocation() {
+		ModelAndView model = new ModelAndView("manager.pickLocation");
+		model.addObject("title", "Welcome");
+		model.addObject("message", "This is default page!");
+		return model;
+	}
+
+}
---
+++ src/main/webapp/WEB-INF/views/jsp/templates/home_body.jsp
@@ -0,0 +1,9 @@
+<%@ taglib prefix="sec" uri="http://www.springframework.org/security/tags" %>
+<div id="content">
+	<div class="center_row col-md-12"><button type="button" class="btn btn-primary">Start</button></div>
+	<div class="center_row col-md-12"><button type="button" class="btn btn-default">Edit Existing</button></div>
+	<sec:authorize access="hasRole('ADMIN')">
+        <div class="center_row col-md-12"><button type="button" class="btn btn-default">Manage</button></div>
+		<div class="center_row col-md-12"><button type="button" class="btn btn-default">Reports</button></div>
+    </sec:authorize>
+</div>
